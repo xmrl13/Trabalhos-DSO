fe9f5361e6b1d92372b6efebe5a740e16ec407af
Gabriel Terra <102969694+GabrielTerra55@users.noreply.github.com>
2022-10-17 21:44:36 -0300

    17/10

---
+++ Codigo/Controller/controller_andar.py
@@ -0,0 +1,18 @@
+from controller_principal import ControllerPrincipal
+
+class ControllerAndar:
+	def __init__(self, controller_principal):
+		self.__controller_princioal = controller_principal
+		self.__andar = []
+	
+	def inclui_andar(self):
+		pass
+	
+	def altera_andar(self):
+		pass
+    
+	def excluir_andar(self):
+		pass
+	
+	def busca_andar(self):
+		pass
