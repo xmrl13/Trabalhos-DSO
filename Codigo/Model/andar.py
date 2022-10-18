50cf0188d5edc5508ec59d150cfa5ea5c37ec936
Gabriel Terra <102969694+GabrielTerra55@users.noreply.github.com>
2022-10-17 21:32:46 -0300

    17/10

--- Codigo/Model/andar.py
+++ Codigo/Model/andar.py
@@ -13,5 +13,20 @@ class Andar:
 	def numero(self,numero):
 		self.__numero = numero
 	
-	def add_quarto(self,quarto: Quarto):
+	@property
+	def quartos(self):
+		return self.__quartos
+
+	def get_quarto_by_numero(self, numero: int): # ficara no controlador de quartos tb
+		for quarto in self.__quartos:
+			if quarto.numero_do_quarto == numero:
+				return quarto
+
+	def add_quarto(self,quarto: Quarto): 
 		self.__quartos.append(quarto)
+	
+	def remove_quarto(self, quarto: Quarto ):   # deve estar no controlador tb e se remover e add em um, deve ter no outro
+		for quarto_1 in self.__quartos:
+			if quarto_1 == quarto:
+				self.__quartos.pop(quarto)
+
