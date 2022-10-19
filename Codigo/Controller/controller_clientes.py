7c12d8a2dba9adb0925c85103c281b343736fe2b
Marcelo de Oliveira <109050359+xmrl13@users.noreply.github.com>
2022-10-18 21:17:15 -0300

    Update de TelaCliente e inicio de ControllerCliente

--- Codigo/Controller/controller_clientes.py
+++ Codigo/Controller/controller_clientes.py
@@ -1,18 +1,24 @@
 from controller_principal import ControllerPrincipal
+from View.tela_clientes import TelaCliente
+from Model.cliente import   Cliente
 
 class ControllerClientes:
     def __init__(self, controller_principal):
         self.__controller_principal = controller_principal
         self.__clientes = []
+        self.__tela_clientes = TelaCliente
+
     
     def inclui_cliente(self):
-        pass
+        dados_cliente = self.__tela_clientes.pega_dados_cliente()
+        cliente = Cliente(dados_cliente['nome_cliente']
+        )
 
     def altera_cliente(self):
-        pass
+        ...
     
     def excluir_cliente(self):
-        pass
+        ...
 
-    def busca_cliente(self):
-        pass
+    def busca_cliente(self): #get cliente by CPF
+        ...
