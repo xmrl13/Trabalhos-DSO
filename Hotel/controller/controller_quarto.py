8004bacb39b12b17b5c08d1ed52202fd4c9849a0
Gabriel Terra <gabrielterra055@gmail.com>
2022-10-24 21:27:51 -0300

    hotel removido

--- Hotel/controller/controller_quarto.py
+++ Hotel/controller/controller_quarto.py
@@ -1,4 +1,4 @@
-from Hotel.view.tela_quarto import TelaQuarto
+from view.tela_quarto import TelaQuarto
 from model.mobilia import Mobilia
 from model.quarto import Quarto
 
@@ -21,14 +21,20 @@ class ControllerQuarto:
     def busca_quarto(self):
         pass
     
+    def lista_quarto(self):
+        pass
+    
+    def retornar(self):
+        self.__controller_principal.abre_tela()
 
+    
     def abre_tela(self):
         lista_opcoes = {
-            1: self.inclui_funcionario,
-            2: self.altera_funcionario,
-            3: self.excluir_funcionario,
-            4: self.busca_funcionario,
-            5: self.lista_funcionario,
+            1: self.inclui_quarto,
+            2: self.altera_quarto,
+            3: self.excluir_quarto,
+            4: self.busca_quarto,
+            5: self.lista_quarto,
             0: self.retornar
         }
         while True:
