26873ce9326d4e9d0d9fe3d637b41dba6a37486a
Gabriel Terra <gabrielterra055@gmail.com>
2022-10-26 12:41:55 -0300

    alterar, buscar, listar (ou)

--- Hotel/controller/controller_quarto.py
+++ Hotel/controller/controller_quarto.py
@@ -3,6 +3,7 @@ from view.tela_quarto import TelaQuarto
 from model.mobilia import Mobilia
 from model.quarto import Quarto
 
+
 class ControllerQuarto:
     def __init__(self, controller_principal):
         self.__controller_principal = controller_principal
@@ -12,12 +13,13 @@ class ControllerQuarto:
     def inclui_quarto(self):
         dados_quarto = self.__tela_quarto.pega_dados_quarto()
         lista_com_dicionario_mobilia = dados_quarto['mobilias']
-        quarto = Quarto(dados_quarto['numero_do_quarto'], dados_quarto['valor_da_diaria'])
+        quarto = Quarto(dados_quarto['numero_do_quarto'],
+                        dados_quarto['valor_da_diaria'])
 
         for dicionario_de_mobilia in lista_com_dicionario_mobilia:
-               quarto.add_mobilia(dicionario_de_mobilia['descricao'], dicionario_de_mobilia['quantidade'])
-        self.__quartos.append(quarto)
-
+            quarto.add_mobilia(
+                dicionario_de_mobilia['descricao'], dicionario_de_mobilia['quantidade'])
+            self.__quartos.append(quarto)
 
     def altera_quarto(self):
         pass
@@ -25,42 +27,44 @@ class ControllerQuarto:
     def excluir_quarto(self):
         if self.lista_quartos is None:
             return
-        quarto = self.busca_quarto_por_numero()
-        if quarto in self.__quartos:
-            self.__quartos.remove(quarto)
-            self.__tela_quarto.quarto_excluido()
-            self.lista_quartos()
+        quarto_recebido = self.__tela_quarto.pega_quarto_por_numero()
+        for quarto in self.__quartos:
+            if quarto.numero_do_quarto == quarto_recebido:
+                self.__quartos.remove(quarto)
+                self.__tela_quarto.quarto_excluido()
+                self.lista_quartos()
+                return
+        self.__tela_quarto.sem_quartos_cadastrados()
 
     def busca_quarto_por_numero(self):
         numero_do_quarto = self.__tela_quarto.pega_quarto_por_numero()
         for quarto in self.__quartos:
             if numero_do_quarto == quarto.numero_do_quarto:
-                return quarto
-    
+                quarto_completo = {
+                    'numero_do_quarto': quarto.numero_do_quarto,
+                    'valor_diaria': quarto.valor_diaria,
+                    'mobilias': quarto.mobilias,
+                }
+                self.__tela_quarto.mostra_quartos(quarto_completo)
+                return
+        self.__tela_quarto.sem_quartos_cadastrados()
+
     def lista_quartos(self):
         if not self.__quartos:
             self.__tela_quarto.sem_quartos_cadastrados()
             return None
-        
-        for quarto in self.__quartos:
-            mobilias_para_tela = []
-            for mobilia in quarto.mobilias:
-                dados_mobilia = {'descricao': mobilia.descricao, 'quantidade': mobilia.quantidade}
-                mobilias_para_tela.append(dados_mobilia)
 
+        for quarto in self.__quartos:
             dados_quarto = {
                 'numero_do_quarto': quarto.numero_do_quarto,
                 'valor_diaria': quarto.valor_diaria,
-                'mobilias': mobilias_para_tela
+                'mobilias': quarto.mobilias,
             }
             self.__tela_quarto.mostra_quartos(dados_quarto)
-            
 
-    
     def retornar(self):
         self.__controller_principal.abre_tela()
 
-    
     def abre_tela(self):
         lista_opcoes = {
             1: self.inclui_quarto,
