58d859da23d66c81015e2f9770cc23e698647461
Gabriel Terra <gabrielterra055@gmail.com>
2022-12-06 19:30:42 -0300

    adicionando DAO em funcionario

---
+++ Hotel/DAO/funcionario_dao.py
@@ -0,0 +1,17 @@
+from Hotel.DAO.dao import Dao
+from Hotel.model.funcionario import Funcionario
+
+class FuncionarioDao(Dao):
+    def __init__(self):
+        super().__init__('funcionarios.pkl')
+
+    def add(self, funcionario: Funcionario):
+        if (isinstance(funcionario, Funcionario)):
+            super().add(funcionario.cracha, funcionario)
+
+    def get(self, key):
+        super().get(key)
+
+    def remove(self, key):
+        super().remove(key)
+
